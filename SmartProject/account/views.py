7a989e32d89c7fedfeacbb3175f01d02ffdca167
Shreya549 <shreya.chatterjee2018@vitstudent.ac.in>
2020-04-06 17:06:05 +0530

    Doctor Login modified

--- SmartProject/account/views.py
+++ SmartProject/account/views.py
@@ -7,21 +7,24 @@ from django.contrib import messages
 def doclogin(request):
 
     if(request.method == 'POST'):
-        email = request.POST['email']
+        username = request.POST['username']
         password = request.POST['password']
-        username = User.objects.get(email=email.lower()).username
+        #username = User.objects.get(email=email.lower()).username
         if (username[:2] == 'Dr'):
             check = True
         else:
             check = False
         if (check):
             user = auth.authenticate(username=username, password = password)
-        if (user is not None):
-            auth.login(request, user)
+            if (user is not None):
+                auth.login(request, user)
             #redirected to home page
-            return render(request, 'DocHome.html')
+                return redirect('/Doctor/Home')
+            else:
+                messages.info(request, "Credentials incorrect")
+                return redirect('doclogin')
         else:
-            messages.info(request, "Credentials incorrect")
+            messages.info(request, "Doctor not found")
             return redirect('doclogin')
 
     else:
